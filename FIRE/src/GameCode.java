47ee76037957913929141729edc6c33e1ca202c2
thanejohannsen <78944212+thanejohannsen@users.noreply.github.com>
2021-02-25 20:56:39 -0600

    Update GameCode.java

--- FIRE/src/GameCode.java
+++ FIRE/src/GameCode.java
@@ -1,4 +1,19 @@
 
 public class GameCode {
-
+  function black(){
+  }
+  function white(){
+  }
+  function red(){
+  }
+  function green(){
+  }
+  function blue(){
+  }
+  function orange(){
+  }
+  function purple(){
+  }
+  function yellow(){
+  }
 }
