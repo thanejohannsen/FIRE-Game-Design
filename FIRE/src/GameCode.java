46195d7093f30ff309631a306132ea6af7e35178
thanejohannsen <78944212+thanejohannsen@users.noreply.github.com>
2021-03-02 21:21:19 -0600

    Update GameCode.java

--- FIRE/src/GameCode.java
+++ FIRE/src/GameCode.java
@@ -3,8 +3,6 @@ public class GameCode {
   
     
  #each function displays corresponding colors 
-  function black(){
-  }
   function white(){
   }
   function red(){
@@ -12,8 +10,6 @@ public class GameCode {
   function green(){
   }
   function blue(){
-  }
-  function orange(){
   }
   function purple(){
   }
@@ -31,30 +27,24 @@ public class GameCode {
   function start(){
     while (i < 10){
       # i is = to the amount of iterations of the game
-      var colorvalue = Math.floor(Math.random() * 8);
+      var colorvalue = Math.floor(Math.random() * 6);
       
       if (colorvalue = 0){
-        black();
-      }
-      else if (colorvalue = 1){
         white();
       }
-      else if (colorvalue = 2){
+      else if (colorvalue = 1){
         red();
       }
-      else if (colorvalue = 3){
+      else if (colorvalue = 2){
         green();
       }
-      else if (colorvalue = 4){
+      else if (colorvalue = 3){
         blue();
       }
-      else if (colorvalue = 5){
-        orange();
-      }
-      else if (colorvalue = 6){
+      else if (colorvalue = 4){
         purple();
       }
-      else if (colorvalue = 7){
+      else if (colorvalue = 5){
         yellow();
       }
       
