4bac3465e0fcd562239338fab16a5f27cdef7f97
thanejohannsen <78944212+thanejohannsen@users.noreply.github.com>
2021-03-03 20:23:43 -0600

    Update GameCode.java

--- FIRE/src/GameCode.java
+++ FIRE/src/GameCode.java
@@ -6,32 +6,27 @@
 
 
 <script>
-import java.io.*;
-import java.util.*;
 
 var canvas = document.getElementById("myCanvas");
 canvas.width  = window.innerWidth;
 canvas.height = window.innerHeight;
 var ctx = canvas.getContext("2d");
 
-var colorlist = ["red","white","green","blue","purple","yellow"]
-
-
 function CanvasColorFill () {
-	chooseRandomColor(scan);
-	if (index == 0) { //red screen
+	var colorvalue = Math.floor(Math.random() * 6);
+	if (colorvalue == 0) { //red screen
 		ctx.fillStyle = "#FF0000";
 		ctx.fillRect(0, 0, canvas.width, canvas.height);
-	} else if (index == 1) { //white screen
+	} else if (colorvalue == 1) { //white screen
 		ctx.fillStyle = "#FFFFFF";
 		ctx.fillRect(0, 0, canvas.width, canvas.height);
-	} else if (index == 2) { //green screen
+	} else if (colorvalue == 2) { //green screen
 		ctx.fillStyle = "#00FF1A";
 		ctx.fillRect(0, 0, canvas.width, canvas.height);
-	} else if (index == 3) { //blue screen
+	} else if (colorvalue == 3) { //blue screen
 		ctx.fillStyle = "#00BBFF";
 		ctx.fillRect(0, 0, canvas.width, canvas.height);
-	} else if (index == 4) { //purple screen
+	} else if (colorvalue == 4) { //purple screen
 		ctx.fillStyle = "#FF00F7";
 		ctx.fillRect(0, 0, canvas.width, canvas.height);
 	} else {  //yellow screen
@@ -41,15 +36,6 @@ function CanvasColorFill () {
 
 }
 
-public static int chooseRandomColor(Scanner scan) {
-	System.out.println("If you want to stop playing hit q, otherwise hit something else: ");
-	String quit = scan.nextLine();
-	while (!quit.equals("q")) {
-		int index = randomGenerator.nextInt(color.size());
-		return index;
-	}
-	return -1;
-}
 
 
 
